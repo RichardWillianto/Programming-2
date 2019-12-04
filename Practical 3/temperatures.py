a260b484b14cfc20f4f3ef1c8a6ed0814e1e6624
ChadWillianto <54535297+ChadWillianto@users.noreply.github.com>
2019-12-04 14:19:18 +0800

    Prac 3

--- Practical 3/temperatures.py
+++ Practical 3/temperatures.py
@@ -1,29 +1,32 @@
-def main() :
+def main():
 
     celc = False
-    while not celc :
-        try :
+    while not celc:
+        try:
             celsius = float(input("Celsius > "))
             celc = True
             print("{0} celsius = {1:.2f} fahrenheit".format(celsius, celsius_to_fahrenheit(celsius)))
-        except ValueError :
+        except ValueError:
             print("Only number accepted")
 
     fahr = False
-    while not fahr :
-        try :
+    while not fahr:
+        try:
             fahrenheit = float(input("Fahrenheit > "))
             fahr = True
             print("{0} fahrenheit = {1:.2f} celsius".format(fahrenheit, fahrenheit_to_celsius(fahrenheit)))
-        except ValueError :
+        except ValueError:
             print("Only number accepted")
 
-def celsius_to_fahrenheit(celsius) :
+
+def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 1.8 + 32
     return fahrenheit
 
-def fahrenheit_to_celsius(fahrenheit) :
+
+def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) / 1.8
     return celsius
+
 
 main()
